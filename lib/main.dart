1243b7c28c13a7667281695fea82563a287c8416
rrosoii <rn7146@edu.turku.fi>
2025-11-03 10:28:49 +0200

    etusivu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'pages/home.dart';
+import 'pages/etusivu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      home: Etusivu(),
     );
   }
 }
