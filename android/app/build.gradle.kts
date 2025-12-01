3d360715be68b71328a54bd079e67695f13f995a
rrosoii <rn7146@edu.turku.fi>
2025-12-01 12:02:30 +0200

    fixed flutter run

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -40,6 +41,8 @@ flutter {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+
     // ⭐ REQUIRED FIREBASE BOM (version manager)
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
 
